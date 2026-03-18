2de6612be67ec066dbf9bb28892afc6982bb4a33
annalovescodingsomushomghaha <lechatlara117@gmail.com>
2026-03-18 11:45:14 +0100

    page modifier profil
    moi anfel a une page qui permettera dans un futur proche de modifier le profil (pseudo, mdp)

--- Code/Main.py
+++ Code/Main.py
@@ -4,6 +4,7 @@ from Page_aide import *
 from Page_paramètres import *
 from Page_Profil import *
 from Page_Calendrier import *
+from Modification import*
 
 def pages():
-    ui.sub_pages({'/': Main_page, '/paramètres': Paramètres, '/aide': Aide, '/profile': Profile, '/calendrier': Calendrier})
+    ui.sub_pages({'/': Main_page, '/paramètres': Paramètres, '/aide': Aide, '/profile': Profile, '/calendrier': Calendrier, '/modification' : Modification})
